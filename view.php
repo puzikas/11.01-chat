8135a8f2bd6c37272a5b89cad4d093cc3e60fefb
Lukas <lukas.chrap@gmail.com>
2018-11-09 19:51:09 +0200

    11.09

--- view.php
+++ view.php
@@ -1,54 +1,3 @@
-<?php
-
-include 'config.php';
-
-try {
-	$conn = new PDO("mysql:host=$domain;dbname=$database", $username, $password);
-	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch(PDOException $e)
-{
-	echo "Connection failed: " . $e->getMessage();
-	die();
-}
-
-$alert = null;
-
-if (isset($_POST['submit'])) {
-
-	if (isset($_POST['username']) && !empty($_POST['username'])) {
-		
-		if (isset($_POST['text']) && !empty($_POST['text'])) {
-
-			$conn->query("INSERT INTO messages (user_id, body) VALUES ('".$_POST['username']."','".$_POST['text']."')");
-
-		} else {
-
-			$alert = '<div class="alert alert-warning" role="alert">
-			Įveskite tekstą!</div>';
-		}
-	} else {
-
-		$alert = '<div class="alert alert-warning" role="alert">
-		Įveskite savo vardą!</div>';
-	}
-}
-
-if (isset($_GET['delete']) && !empty($_GET['delete'])) {
-	$stmt = $conn->prepare("DELETE FROM messages WHERE id = :id");
-	$stmt->execute(['id' => $_GET['delete']]);
-}
-
-
-
-$stmt = $conn->query("select messages.id, nickname, body, created_on from messages
-join users ON users.id = messages.user_id
-");
-$messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-
-?>
-
 <!DOCTYPE html>
 <html>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
@@ -67,10 +16,10 @@ $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 			foreach ($messages as $message) {
 	echo $message['created_on'] . " "
-	 . $message['nickname']
+	 . $message['name']
 	 . " said: "
 	 . $message['body']
-	 . " <a href='index.php?delete=" . $message['id'] . "'>X</a>"
+	 
  	 . "<br>";
 }
 
@@ -96,6 +45,7 @@ $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
 				</div>
 			</div>
 		</form>
+		
 	</div>
 </body>
 </html>
